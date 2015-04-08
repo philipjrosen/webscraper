aa35e58e00e952d427ac37abf5466148853130c3
Philip Rosen <philipjrosen@gmail.com>
2015-04-08 16:34:52 -0700

    adds toggle between grid view and list view using ui-router states

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,12 +1,25 @@
 angular.module('app')
 .config(function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/events');
+  $urlRouterProvider.otherwise('/events/list');
 
   $stateProvider
     .state('events', {
       url: '/events',
-      templateUrl: 'app/components/events/eventsView.html',
+      templateUrl: 'app/components/events/eventsView.html'
+    })
+
+    $stateProvider
+    .state('events.list', {
+      url: '/list',
+      templateUrl: 'app/components/events/listView.html',
+      controller: 'EventsCtrl'
+    })
+
+    $stateProvider
+    .state('events.grid', {
+      url: '/grid',
+      templateUrl: 'app/components/events/gridView.html',
       controller: 'EventsCtrl'
     });
 });
