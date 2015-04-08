225b06d1f313ecf5f83c8e61387f28a302875050
Philip Rosen <philipjrosen@gmail.com>
2015-04-08 11:27:03 -0700

    retrieves html from url

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var app = express();
 
 app.use(express.static(__dirname + ''));
 
+app.get('/events', function (req, res) {
+  var url = 'http://events.stanford.edu/2014/October/1/';
+
+  request(url, function (error, response, html) {
+    if (error) {
+      console.log("error:", error);
+    }
+    console.log(html);
+  });
+});
+
+
 var port = '3000';
 
 app.listen(port, function() {
